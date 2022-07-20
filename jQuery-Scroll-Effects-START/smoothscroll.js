106e36cc6bcf41903f9266e05d063af60eadf67f
Johnny_W <101821626+johnnyw01@users.noreply.github.com>
2022-07-20 17:12:02 -0500

    Add files via upload

---
+++ jQuery-Scroll-Effects-START/smoothscroll.js
@@ -0,0 +1,63 @@
+$("nav ul li a").click(function() {
+    const thisSection = $(this).attr("href");
+    const thisLink = $(this);
+
+    $("html, body").stop().animate({
+        scrollTop: $(thisSection).offset().top - 200
+    }, 800, "easeOutCirc");
+    return false;
+}); //end of clickhandler
+
+$(window).on("load", function() {
+    const allLinks = $("nav ul li a");
+    const posts = $("section");
+    let pageTop;
+    let postPos;
+    let counter = 0;
+    let prevCounter = 0;
+    let doneResizing;
+    let postTops = [];
+    resetPagePosition();
+
+    //console.log(postTops);
+
+    $(window).scroll(function() {
+        pageTop = $(window).scrollTop() + 210;
+        if (pageTop > postTops[counter + 1]) {
+            counter++;
+            //console.log(`scrolling down ${counter}`);
+        } else if (counter > 0 && pageTop < postTops[counter]) {
+            counter--;
+            //console.log(`scrolling up ${counter}`);
+        }
+        if (counter != prevCounter) {
+            $(allLinks).removeAttr("class");
+            $("nav ul li a").eq(counter).addClass("selected");
+            prevCounter = counter;
+        }
+    });
+
+    $(window).on("resize", function() {
+        clearTimeout(doneResizing);
+        doneResizing = setTimeout(function() {
+            resetPagePosition();
+        }, 500);
+    });
+
+    function resetPagePosition() {
+        postTops = [];
+        posts.each(function() {
+            postTops.push(Math.floor($(this).offset().top));
+        });
+        const pagePosition = $(window).scrollTop() + 210;
+        counter = 0;
+        for (let i = 0; i < postTops.length; i++) {
+            if (pagePosition > postTops[i]) {
+                counter++;
+            }
+        }
+        counter--;
+        $(allLinks).removeAttr("class");
+        $("nav ul li a").eq(counter).addClass("selected");
+    }
+});
